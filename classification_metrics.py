991369dba1c4c2345d59bc36ee65b5b5e87b4178
Walt9819 <walter22r@icloud.com>
2022-04-23 13:47:19 -0500

    Inherit from parent metrics class the ability to pass a model and perform on it the computation. Otherwise with alone values should be able to compute metrics

--- classification_metrics.py
+++ classification_metrics.py
@@ -1,34 +1,14 @@
-class ModelMetricsCalculator(MetricsCalculator):
-    """Compute most common performance metrics for a binary
-    or a multiple classification model.
-    Parameters
-    ----------
-    classifier : tensorflow classification model
-        Trained classifier model to be used for predictions.
-    X : array-like, or tensor
-        Inputs to be passed to the classifier.
-    Y : array-like, or tensor
-        Target labels to be predicted by the classifier.
-    """
-    
-    def __init__(self, classifier, X, Y):
-        import sklearn.metrics as metrics
+class MetricsCalculator():
+    def __init__(self, Y_labels, Y_probabilityPreds):
         import numpy as np
-        self._model = classifier # model to predict
-        self._model.trainable = False # freeze layer weights
-        self._X = X # input data
-        self._Y = Y # predictions
-        self._Make_predictions() # perform predictions (probability and label)
-        self._confusion_matrix = metrics.confusion_matrix(self._Y, self._labelPredictions)
+        import sklearn.metrics as metrics
+        self._Y = y_labels # correct labels
+        self._probabilityPredictions = Y_probabilityPreds # predictions with probability distribution
         self._unique_labels = np.unique(self._Y)
-        
-    # Predict classifier outputs
-    def _Make_predictions(self):
-        self._probabilityPredictions = model.predict(self._X)
         self._labelPredictions = tf.math.argmax(self._probabilityPredictions, axis=1).numpy()
-    
-
-    # Compute confusion matrix for specific label if given
+        self._confusion_matrix = metrics.confusion_matrix(self._Y, self._labelPredictions)
+        
+        # Compute confusion matrix for specific label if given
     def Confusion_matrix(self, target_label_index=None, return_type="matrix"):
         """Computes a binary confusion matrix for `target_label` if given.
         Otherwise returns the multi-class confusion matrix.
@@ -94,3 +74,24 @@ class ModelMetricsCalculator(MetricsCalculator):
                 "FP": FP,
                 "TN": TN
             }
+
+
+class ModelMetricsCalculator(MetricsCalculator):
+    """Compute most common performance metrics for a binary
+    or a multiple classification model.
+    Parameters
+    ----------
+    classifier : tensorflow classification model
+        Trained classifier model to be used for predictions.
+    X : array-like, or tensor
+        Inputs to be passed to the classifier.
+    Y : array-like, or tensor
+        Target labels to be predicted by the classifier.
+    """
+    
+    def __init__(self, classifier, X, Y):
+        import sklearn.metrics as metrics
+        import numpy as np
+        classifier.trainable = False # freeze layer weights
+        probabilityPredictions = model.predict(X)
+        super().__init__(Y, probabilityPredictions) # init from parent class
